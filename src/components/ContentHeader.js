7c48f85078a68b38be6e9c58e562e3c93666065b
Talis Lazdins <lazdinst@gmail.com>
2017-10-27 12:20:05 -0700

    Commiting

--- src/components/ContentHeader.js
+++ src/components/ContentHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash'
 import { summaryContent, profileImage } from '../data/summary';
-
+import Space from './Space'
 import {
   Container, Divider, Dropdown, Grid, Header, Icon, Image, List, Menu, Segment, Visibility,
 } from 'semantic-ui-react'
@@ -45,7 +45,8 @@ export default class ContentHeader extends Component {
     const summaryText = summaryContent.map((content) => {return <div><p style={{paddingBottom: '1rem'}}>{content}</p></div>});
     return (
       <div>
-        <div class="bgimg-1">
+        <div class="space">
+          <Space/>
           <Container text>
             <Header as='h2'>Talis Lazdins</Header>
             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
@@ -63,7 +64,7 @@ export default class ContentHeader extends Component {
           <div className="paralax-title">Parallax Demo</div>
           <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
         </div>
-        <div className="bgimg-2">
+        <div className="city-skyline">
           <div className="caption">
             <span className="border" style={{backgroundColor: "transparent", fontSize: "25px", color: "#f7f7f7"}}>LESS HEIGHT</span>
           </div>
---
+++ src/components/Space.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Container, Divider, Dropdown, Grid, Header, Icon, Image, List, Menu, Segment, Visibility,
+} from 'semantic-ui-react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div className='stars'></div>
+        <div className='stars2'></div>
+        <div className='stars3'></div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
